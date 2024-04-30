c3bf0b0b6fe02b39d9f3f3eba5c7777b68434d04
Temirlan <tzhanshin@mail.ru>
2024-04-30 20:43:38 +0500

    upd

--- lab 11/phonebook2.py
+++ lab 11/phonebook2.py
@@ -25,13 +25,14 @@ def menu():
     print('''
         1 - show all users
         2 - upload data from csv
-        3 - add user from console
+        3 - insert user(update phone if exist)
         4 - update phone number
         5 - sort by name
         6 - sort by surname
-        7 - search user
-        8 - delete user
+        7 - search user by pattern
+        8 - delete user by name or phone
         9 - insert by list
+        10 - get users with pagination
             ''')
     num = input("enter number: ")
     if num == '1':
@@ -39,7 +40,27 @@ def menu():
     elif num == '2':
         upload()
     elif num == '3':
-        add()
+        newname = input("Enter name: ")
+        newsurname = input("Enter surname: ")
+        newphone = input("Enter phone: ")
+        cur.execute(""" CREATE OR REPLACE PROCEDURE insert_user(IN new_name VARCHAR, IN new_surname VARCHAR, IN new_phone VARCHAR)
+                        AS $$
+                        BEGIN
+                            IF EXISTS (SELECT 1 FROM phonebook2 WHERE name = new_name AND surname = new_surname) THEN 
+                                UPDATE phonebook2 SET phone = new_phone WHERE name = new_name AND surname = new_surname;
+                            ELSE
+                                INSERT INTO phonebook2 (name, surname, phone) VALUES (new_name, new_surname, new_phone);
+                            END IF;
+                        END;
+                        $$ LANGUAGE plpgsql;
+
+                        CALL insert_user(%s, %s, %s);""", (newname, newsurname, newphone))
+        conn.commit()
+        print("success")            
+
+        if input("\n0 - return to menu: ") == '0':
+            menu()
+
     elif num == '4':
         update()
     elif num == '5':
@@ -47,11 +68,55 @@ def menu():
     elif num == '6':
         sortbysurname()
     elif num == '7':
-        search()
-    elif num == '8':
-        delete()
+        pattern = input("enter a pattern: ")
+        word = '%' + pattern + '%'
+        cur.execute(""" CREATE OR REPLACE FUNCTION search_users(pattern VARCHAR)
+                        RETURNS SETOF phonebook2 
+                        AS $$
+                        BEGIN
+                            RETURN QUERY SELECT * FROM phonebook2 WHERE name ILIKE pattern OR surname ILIKE pattern OR phone ILIKE pattern;
+                        END;
+                        $$ LANGUAGE plpgsql;
+                                    
+                        SELECT * FROM search_users(%s);""", (word, ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        if input("\n0 - return to menu: ") == '0':
+            menu()
+    elif num == '8':    
+        data = input("enter name or phone: ")
+        cur.execute(''' CREATE OR REPLACE PROCEDURE delete_user(data VARCHAR)
+                        AS $$
+                        BEGIN
+                            DELETE FROM phonebook2 WHERE name = data OR phone = data;
+                        END;
+                        $$ LANGUAGE plpgsql;
+
+                        CALL delete_user(%s);''', (data, ))
+        conn.commit()
+        print("deleted")
+        if input("\n0 - return to menu: ") == '0':
+            menu()
+        
     elif num == '9':
         insertbylist()
+    elif num == '10':
+        a, b = map(int, input("LIMIT, OFFSET: ").split())
+        cur.execute(''' CREATE OR REPLACE FUNCTION get_users(IN "limit" INTEGER, IN "offset" INTEGER)
+                        RETURNS SETOF phonebook2 
+                        AS $$
+                        BEGIN
+                            RETURN QUERY SELECT * FROM phonebook2 ORDER BY id LIMIT "limit" OFFSET "offset";
+                        END;
+                        $$ LANGUAGE plpgsql;
+
+                        SELECT * FROM get_users(%s, %s);''', (a, b))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        if input("\n0 - return to menu: ") == '0':
+            menu()
 
 def show():
     cur.execute('SELECT * FROM phonebook2')
@@ -74,22 +139,22 @@ def upload():
         if input("\n0 - return to menu: ") == '0':
             menu()
 
-def add():
-    print("enter data for new user by this order -> name, surname, phone):")
-    new_user = list(map(str, input().split()))  
+# def add():
+#     print("enter data for new user by this order -> name, surname, phone):")
+#     new_user = list(map(str, input().split()))  
 
-    if not exist(new_user):
-        cur.execute('INSERT INTO phonebook2 (name, surname, phone) VALUES (%s, %s, %s)', new_user)
-        print("added new user")
-        conn.commit()
-    elif exist(new_user):
-        cur.execute('UPDATE phonebook2 SET phone = %s WHERE surname = %s AND name = %s', (new_user[2], new_user[1], new_user[0]))
-        print(f"updated phone number for {new_user[0]}")
-    else:
-        print("this user already exists")
+#     if not exist(new_user):
+#         cur.execute('INSERT INTO phonebook2 (name, surname, phone) VALUES (%s, %s, %s)', new_user)
+#         print("added new user")
+#         conn.commit()
+#     elif exist(new_user):
+#         cur.execute('UPDATE phonebook2 SET phone = %s WHERE surname = %s AND name = %s', (new_user[2], new_user[1], new_user[0]))
+#         print(f"updated phone number for {new_user[0]}")
+#     else:
+#         print("this user already exists")
 
-    if input("\n0 - return to menu: ") == '0':
-        menu()
+#     if input("\n0 - return to menu: ") == '0':
+#         menu()
 
 def update():
     print("enter name and new phone number:")
@@ -128,26 +193,26 @@ def sortbysurname():
     if input("\n0 - return to menu: ") == '0':
         menu()
 
-def search():
-    mypattern = input("enter pattern of name/surname/phone to find users: ")
-    mode = input("search by name/surname/phone? ")
-    if mode == "name":
-        cur.execute('SELECT * FROM phonebook2 WHERE name ILIKE %s', (f'%{mypattern}%', ))
-    elif mode == "surname":
-        cur.execute('SELECT * FROM phonebook2 WHERE surname ILIKE %s', (f'%{mypattern}%', ))
-    elif mode == "phone":
-        cur.execute('SELECT * FROM phonebook2 WHERE phone LIKE %s', (f'%{mypattern}%', ))
+# def search():
+#     mypattern = input("enter pattern of name/surname/phone to find users: ")
+#     mode = input("search by name/surname/phone? ")
+#     if mode == "name":
+#         cur.execute('SELECT * FROM phonebook2 WHERE name ILIKE %s', (f'%{mypattern}%', ))
+#     elif mode == "surname":
+#         cur.execute('SELECT * FROM phonebook2 WHERE surname ILIKE %s', (f'%{mypattern}%', ))
+#     elif mode == "phone":
+#         cur.execute('SELECT * FROM phonebook2 WHERE phone LIKE %s', (f'%{mypattern}%', ))
 
-    rows = cur.fetchall()
+#     rows = cur.fetchall()
 
-    if rows:
-        for row in rows:
-            print(row)
-    else:
-        print(f"'{mypattern}' not found")
+#     if rows:
+#         for row in rows:
+#             print(row)
+#     else:
+#         print(f"'{mypattern}' not found")
 
-    if input("\n0 - return to menu: ") == '0':
-        menu()
+#     if input("\n0 - return to menu: ") == '0':
+#         menu()
     
 def delete():
     mode = input("delete by name/phone: ")
